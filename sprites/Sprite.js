cb03b17b321ba3af1f289d53f69e5c78b5494ef3
Hon Lam <hon@junglescout.com>
2018-09-19 22:05:43 -0700

    Added classes Sprite and Hero.

---
+++ sprites/Sprite.js
@@ -0,0 +1,20 @@
+const Sprite = class {
+  constructor(options) {
+    const {
+      imageSet,
+      speed,
+      movementPattern,
+      positionInitial
+    } = options
+  }
+
+  move() {
+
+  }
+
+  imageChange(){
+
+  }
+}
+
+export default Sprite
---
+++ sprites/heroes/Hero.js
@@ -0,0 +1,16 @@
+import Sprite from '../Sprite.js'
+
+const Hero = class extends Sprite {
+  constructor(options){
+    super(options)
+
+    const {
+      name,
+      health
+    } = options
+  }
+
+  healthChange() {
+    
+  }
+}
